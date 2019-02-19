254111ae8ffb9f080d60704fed2984461bbc6950
Serge Uniquite <sergeuniquite@gmail.com>
2019-02-19 17:54:29 +0300

    crop-counter-example

--- src/App.js
+++ src/App.js
@@ -2,27 +2,54 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const
+	_m = 'http://developer.mozilla.org/static/img/beast-404.ce38fcf80386.png',
+	_a = 'https://alterainvest.ru/upload/pic_alt3/21299__2e6f531c-6cc1-4457-9240-a94612e02c89/dog.png';
+
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+	constructor() {
+		super();
+		
+		this.startDownload = this.startDownload.bind(this);
+		this.finishDownload = this.finishDownload.bind(this);
+	}
+	
+	componentDidMount() {
+		this.startDownload(_m);
+	}
+	
+	startDownload(src) {
+		const downloadedImg = new Image;
+		
+		downloadedImg.crossOrigin = "Anonymous";
+		downloadedImg.addEventListener("load", this.finishDownload, false);
+		downloadedImg.src = src;
+	}
+	
+	finishDownload({ path }) {
+		const
+			{ canvas, app } = this.refs,
+			img = path[0],
+			context = canvas.getContext('2d');
+			
+		context.drawImage(img, -50, 0);
+		
+		const src = canvas.toDataURL();
+		
+		const image = new Image;
+		
+		image.src = src;
+		
+		app.appendChild(image);
+	}
+	
+	render() {
+		return (
+			<div ref="app" className="App">
+				<canvas id="canvas" ref="canvas" width="200" height="400" />
+			</div>
+		);
+	}
 }
 
 export default App;
