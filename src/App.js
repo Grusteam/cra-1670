1f687f7026488a9a2924146eaa26a2fd4b721d4a
Serge Uniquite <sergeuniquite@gmail.com>
2019-03-06 13:21:27 +0300

    simple-video

--- src/App.js
+++ src/App.js
@@ -6,7 +6,12 @@ class App extends Component {
 	render() {
 		return (
 			<div className="App">
-				App
+				{/*<video preload="auto" muted controls={true} >*/}
+				<video preload="auto" muted playsInline autoPlay="autoplay" loop="loop">
+				  <source src="./360_25_100_h264.mp4" type="video/mp4" />
+				  <source src="./360_25_100_vp8.webm" type="video/webm" />
+				  Your browser doesn't support HTML5 video tag.
+				</video>
 			</div>
 		);
 	}
